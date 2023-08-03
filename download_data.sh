14a93709c9376633c2cd00d8d12cf1a738b5b4e7
Fish-Home on WSL2 <suzuki.t.dq@m.titech.ac.jp>
2023-08-04 04:01:05 +0900

    update

--- download_data.sh
+++ download_data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # download and extract the CrossDocked2020 molecular structures
-wget https://bits.csb.pitt.edu/files/crossdock2020/CrossDocked2020_v1.1.tgz -P data/crossdock2020/
-tar -C data/crossdock2020/ -xzf data/crossdock2020/CrossDocked2020_v1.1.tgz
+wget https://bits.csb.pitt.edu/files/crossdock2020/v1.1/CrossDocked2020_v1.1.tgz -P data/crossdock2020/
+tar -C data/crossdock2020/ -xzf data/crossdock2020/v1.1/CrossDocked2020_v1.1.tgz
 
 # download the train and test sets
 wget https://bits.csb.pitt.edu/files/it2_tt_0_lowrmsd_mols_train0_fixed.types -P data/crossdock2020/
